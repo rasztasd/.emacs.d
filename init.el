878a7dae93def7d184ece553131475a432021730
rasztasd <rasztasd@gmail.com>
2011-05-18 22:34:37 +0200

    Initial commit - Php mode, yasnippet

---
+++ init.el
@@ -0,0 +1,10 @@
+(add-to-list 'load-path "~/.emacs.d/vendor")
+(progn (cd "~/.emacs.d/vendor")
+       (normal-top-level-add-subdirs-to-load-path))
+;PHP
+(require 'php-mode)
+
+;Yasnippet
+(require 'yasnippet)
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/vendor/yasnippet-0.6.1c/snippets")
