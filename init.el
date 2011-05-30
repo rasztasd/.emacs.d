7791c520d8b47c62fc9e9bd232cb7c481c6001ab
rasztasd <rasztasd@rasztasd-ubuntu.(none)>
2011-05-30 02:06:53 +0200

    Setup files for python and autocomplete, and init.el for python and autocomplete
    Python setup scripts: pymacs ropemacs, rope, ropemode

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;Add vendor ~/.emacs.d/vendor subdirectories to the load-path
 (add-to-list 'load-path "~/.emacs.d/vendor")
 (progn (cd "~/.emacs.d/vendor")
        (normal-top-level-add-subdirs-to-load-path))
@@ -8,4 +9,17 @@
 (require 'yasnippet)
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/vendor/yasnippet-0.6.1c/snippets")
-(yas/load-directory "~/.emacs.d/minimal-yasnippet-php-mode")
+;(yas/load-directory "~/.emacs.d/minimal-yasnippet-php-mode")
+
+;Autocomplete
+(add-to-list 'load-path "~/.emacs.d/autocomplete/")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/autocomplete/ac-dict")
+(ac-config-default)
+
+;Pymacs
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
